e9f29f8913d4f6a0eac5ca57622d8c9cc231a4fd
Adrian Galindo <75171829+werty4777@users.noreply.github.com>
2021-01-14 18:43:40 -0500

    Update almacenController.java

--- src/main/java/com/argo/gateway/area/infrastructure/almacenController.java
+++ src/main/java/com/argo/gateway/area/infrastructure/almacenController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@RequestMapping({"/inventario","/api/inventario"})
 public class almacenController {
 
     @Autowired
